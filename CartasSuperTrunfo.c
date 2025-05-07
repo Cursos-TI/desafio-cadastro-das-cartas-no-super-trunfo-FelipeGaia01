0cd815168b0475eca76c1290f58097231ebab84b
FelipeGaia01 <felipegaia40@gmail.com>
2025-05-07 23:18:59 +0000

    criei os super poderes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,12 +15,11 @@ int main() {
     int pontos_turisticos1;
 
     // calculando densidade populacional e pir per capita carta 01
-    int densidade1 =  populacao1 / tamanho1;
-    int pibpercapita1 =  pib1 / populacao1;
 
+    float densidade1, pibpercapita1;
+    
 
-
-    // carta 02 - declarando variaveis
+    //carta 02 - declarando variaveis
     char estado2;
     char codigo2[4];
     char cidade2[50];
@@ -29,13 +28,11 @@ int main() {
     float pib2;
     int pontos_turisticos2;
 
-     // calculando densidade populacional e pir per capita carta 02
 
+    // calculando densidade populacional e pir per capita carta 02
 
-
-
-
-
+    float densidade2, pibpercapita2;
+    float super_poder_carta1, super_poder_carta2;
   
     //obtendo dados da carta 01
 
@@ -85,6 +82,16 @@ int main() {
     printf("Pontos turísticos: \n");
     scanf("%d", &pontos_turisticos2);
     
+    densidade1 = (float) populacao1/tamanho1;
+    pibpercapita1 = (float) pib1/populacao1;
+
+    super_poder_carta1 = (float) populacao1 + tamanho1 + pib1 + pontos_turisticos1 + pibpercapita1 + densidade1;
+
+    densidade2 = (float) populacao2/tamanho2;
+    pibpercapita2 = (float) pib2/populacao2;
+
+    super_poder_carta2 = (float) populacao2 + tamanho2 + pib2 + pontos_turisticos2 + pibpercapita2 + densidade2;
+
     // Imprimindo dados da carta 01
 
     printf("\nCarta 1:\n");
@@ -95,6 +102,11 @@ int main() {
     printf("Área: %.3f km²\n", tamanho1);
     printf("PIB: %.3f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+
+    printf("densidade: %.3f\n", densidade1);
+    printf("pib_per_capita: %.3f\n", pibpercapita1);
+    
+    printf("super poder carta 1: %.3f\n", super_poder_carta1);
 
     // Imprimindo dados da carta 02
 
@@ -107,9 +119,10 @@ int main() {
     printf("PIB: %.3f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
 
-    // imprimido densidade populacional e pir per capita carta 01
-    printf("densidade: %.3f\n", densidade1);
-    printf("pib_per_capita: %.3f\n", pibpercapita1);
+    printf("densidade: %.3f\n", densidade2);
+    printf("pib_per_capita: %.3f\n", pibpercapita2);
+
+    printf("super poder carta 2: %.3f\n", super_poder_carta2);
 
     return 0;
 }
